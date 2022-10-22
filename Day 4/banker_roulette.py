e1faa0c200627bce45e91a375e0124610d5d383e
Malcolm Miller <entid@protonmail.com>
2022-10-22 15:20:13 +1100

    completed project rock paper scissors with art

--- Day 4/banker_roulette.py
+++ Day 4/banker_roulette.py
@@ -2,8 +2,11 @@ import random
 
 names_string = input("Give me everybody's names, separated by a comma: ")
 
+# split the names_string into a list of names
 names = names_string.split(", ")
 
+# generate a random integer between 0 and the length of names list
+# -1 for index range of list
 choice = random.randint(0, (len(names)-1))
 
 print(names[choice])
--- Day 4/project_rock_paper_scissors.py
+++ Day 4/project_rock_paper_scissors.py
@@ -1,18 +1,39 @@
 import random
+# import ascii art
+import rock_paper_scissors as rps
 
+# set a list of possible choices
 hand = [0, 1, 2]
+# make a choice randomly for the computer
 computer_choice = random.choice(hand)
 
-choice = input(
-    "What is your choice? Choose 0 for Rock\nchoose 1 for Paper\nchoose 2 for Scissors")
+# get the users choice and convert to integer
+choice = int(input(
+    "What is your choice? Choose 0 for Rock, choose 1 for Paper, choose 2 for Scissors: "))
+if choice >= 3:
+    print("You didn't choose 0, 1 or 2 did you? You lose.")
+else:
+    # print the ascii art of the users choice
+    print(f"You chose {rps.art[choice]}")
+    # print the ascii art of the computer's choice
+    print(f"Computer chose {rps.art[computer_choice]}")
 
-
-print(choice)
-print(f"Computer chose {computer_choice}")
-
-if choice == 0 and computer_choice == 1:
-    print("You lose")
-elif choice == 0 and computer_choice == 2:
-    print("You win")
-elif choice == 0 and computer_choice == 0:
-    print("It's a draw")
+    # set the conditions for win, lose or draw
+    if choice == 0 and computer_choice == 1:
+        print("You lose")
+    elif choice == 0 and computer_choice == 2:
+        print("You win")
+    elif choice == 0 and computer_choice == 0:
+        print("It's a draw")
+    elif choice == 1 and computer_choice == 0:
+        print("You win")
+    elif choice == 1 and computer_choice == 1:
+        print("It's a draw")
+    elif choice == 1 and computer_choice == 2:
+        print("You lose")
+    elif choice == 2 and computer_choice == 0:
+        print("You lose")
+    elif choice == 2 and computer_choice == 1:
+        print("You win")
+    elif choice == 2 and computer_choice == 2:
+        print("It's a draw")

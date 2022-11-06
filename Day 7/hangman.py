88ee55dce5542cc8e0e772e2adfeb15ef94fd17c
Malcolm Miller <entid@protonmail.com>
2022-11-06 15:35:26 +1100

    hangman challenge 3

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -12,11 +12,20 @@ for letter in word_choice:
     display.append("_")
 print(display)
 
-guess = input("Guess a letter: ").lower()
+game_on = True
+while game_on == True:
+    guess = input("Guess a letter: ").lower()
 
-i = 0
-for letter in word_choice:
-    if guess == letter:
-        display[i] = letter
-    i += 1
-print(display)
+    i = 0
+    for letter in word_choice:
+        if guess == letter:
+            display[i] = letter
+        i += 1
+    print(display)
+
+    if "_" not in display:
+        game_one = False
+        print("You win!")
+        break
+    else:
+        continue
